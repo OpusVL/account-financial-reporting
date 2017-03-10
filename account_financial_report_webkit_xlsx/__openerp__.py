706276bf0ebf3b28c090f449cd0cea4e83acb723
Nick Booker <nick.booker@opusvl.com>
2017-03-10 11:23:16 +0000

    Add OpusVL to the list of authors.

--- account_financial_report_webkit_xlsx/__openerp__.py
+++ account_financial_report_webkit_xlsx/__openerp__.py
@@ -23,7 +23,7 @@
     'name': 'Add XLS export to accounting reports',
     'version': '8.0.0.4.0',
     'license': 'AGPL-3',
-    'author': "Noviat,Odoo Community Association (OCA)",
+    'author': "OpusVL,Noviat,Odoo Community Association (OCA)",
     'category': 'Generic Modules/Accounting',
     'description': """
 
